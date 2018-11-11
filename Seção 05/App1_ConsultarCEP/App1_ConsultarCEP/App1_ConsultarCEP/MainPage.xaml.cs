4b217653a9d0fff906b797513831ce64eb7343ea
Renan Milfont <renan.milfont@gmail.com>
2018-11-11 13:31:36 -0200

    App01 - Concluído

---
+++ Seção 05/App1_ConsultarCEP/App1_ConsultarCEP/App1_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App1_ConsultarCEP.Servico.Modelo;
+using App1_ConsultarCEP.Servico;
+
+namespace App1_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Botao.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            String cep = CEP.Text.Trim();
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            Resultado.Text = string.Format("Endereço: {2} de {3}, {0}, {1}", end.localidade, end.uf, end.logradouro, end.bairro);
+        } 
+
+    }
+}
